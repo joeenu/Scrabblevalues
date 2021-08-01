9a9151b4dcdfd7bd8b1df3c8599ce8de2ce66e91
LarsZauberer <wasser.ian@gmail.com>
2021-08-01 22:22:38 +0200

    Functions preparing for multithreading

---
+++ lib.py
@@ -0,0 +1,44 @@
+def calcScore(line):
+    a = line.count("a")
+    b = line.count("b")
+    c = line.count("c")
+    d = line.count("d")
+    e = line.count("e")
+    f = line.count("f")
+    g = line.count("g")
+    h = line.count("h")
+    i = line.count("i")
+    j = line.count("j")
+    k = line.count("k")
+    l = line.count("l")
+    m = line.count("m")
+    n = line.count("n")
+    o = line.count("o")
+    p = line.count("p")
+    q = line.count("q")
+    r = line.count("r")
+    s = line.count("s")
+    t = line.count("t")
+    u = line.count("u")
+    v = line.count("v")
+    w = line.count("w")
+    x = line.count("x")
+    y = line.count("y")
+    z = line.count("z")
+    ä = line.count("ä")
+    ö = line.count("ö")
+    ü = line.count("ü")
+    return (e+ n+ s+ i+ r+ t+ u+ a+ d) + (h+ g+ l+ o)*2 + (m+ b+ w+ z)*3 + (c+ f+ k+ p)*4 + (ä+ j+ ü+ v)*6 + (ö+ x)*8 + (q+ x)*10
+
+
+def calcScoreInRange(arr, low, high, data):
+    values = []
+    for i in range(low, high):
+        l = arr[i].lower()
+        l = l.replace("\n", "")
+        values.append(l + " " + str(calcScore(l)))
+
+    # Couldn't concatinate lists with + because of passing the value through reference
+    for i in values:
+        data.append(i)
+        
--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 from rich.progress import track
+import threading as t
+
+from lib import calcScoreInRange
+
+THREADS = 10
 
 
 def getscore(ele):
@@ -26,42 +31,22 @@ with open("words.txt", "r+") as f:
 with open("words.txt", "r+") as fi:
     lines = fi.readlines()
     fi.seek(0)
-    for ii in track(range(len(lines))):
-        data = lines[ii].lower()
-#        print(repr(data))
-        data = data.replace("\n","")
-#        print(repr(data))
-        a = data.count("a")
-        b = data.count("b")
-        c = data.count("c")
-        d = data.count("d")
-        e = data.count("e")
-        f = data.count("f")
-        g = data.count("g")
-        h = data.count("h")
-        i = data.count("i")
-        j = data.count("j")
-        k = data.count("k")
-        l = data.count("l")
-        m = data.count("m")
-        n = data.count("n")
-        o = data.count("o")
-        p = data.count("p")
-        q = data.count("q")
-        r = data.count("r")
-        s = data.count("s")
-        t = data.count("t")
-        u = data.count("u")
-        v = data.count("v")
-        w = data.count("w")
-        x = data.count("x")
-        y = data.count("y")
-        z = data.count("z")
-        ä = data.count("ä")
-        ö = data.count("ö")
-        ü = data.count("ü")
-        value = (e+ n+ s+ i+ r+ t+ u+ a+ d) + (h+ g+ l+ o)*2 + (m+ b+ w+ z)*3 + (c+ f+ k+ p)*4 + (ä+ j+ ü+ v)*6 + (ö+ x)*8 + (q+ x)*10
-        fi.write(data + " " + str(value) + "\n")
+    
+    packages = [i*len(lines)/THREADS for i in range(THREADS)]
+    data = []
+    threads = []
+    calcScoreInRange(lines, 0, len(lines), data)
+    """ for i in track(range(THREADS), description="Starting Threads"):
+        threads.append(t.Thread(target=calcScoreInRange, args=(lines, int(packages[i]), int(packages[i]+len(lines)), data)))
+        threads[i].start()
+    for i in track(threads, description="Waiting for Threads"):
+        i.join() """
+    # Ensuring the same output as the other version
+    data = "\n".join(data)
+    data = data[-1].replace("\n", "")
+    
+    # Writing
+    fi.writelines(data)
     fi.truncate()
 
 # Sort the file
